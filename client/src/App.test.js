751134a81d4fc499d63bc3304168426a4840ccf7
David White <dwhite0627@gmail.com>
2020-02-21 21:14:00 -0500

    Add test to ensure App renders in App.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { fireEvent, render, cleanup, act } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('renders the App div', () => {
+  // Arrange
+  const { getByTestId } = render(<App />)
+
+  // Act
+  const appDiv = getByTestId(/App-div/i)
+
+  // Assert
+  expect(appDiv).toBeInTheDocument();
+})
