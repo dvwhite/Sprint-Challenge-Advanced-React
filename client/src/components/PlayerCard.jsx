5b6dc366d1ec615b593045b196586842eca663e4
David White <dwhite0627@gmail.com>
2020-02-21 20:13:30 -0500

    Style the API data as cards, set background image

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  margin: 1%;
+  background-color: #333;
+  color: #fff;
+  opacity: 0.95;
+  border: 0;
+  border-radius: 10px;
+  box-shadow: 0 -1px 0 #e0e0e0, 
+              0 0 2px rgba(0, 0, 0, 0.12), 
+              0 2px 4px rgba(0, 0, 0, 0.24);
+  width: 18rem;
+  height: 12rem;
+  overflow: hidden;
+  word-wrap: break-word;
+`
+
+/*
+Player shape should look like:
+  {
+    name: "Alex Morgan"
+    country: "United States"
+    searches: 100
+    id: 0
+  }
+*/
 
 const PlayerCard = ({ data }) => {
   return (
-    <div>
-      <h2>Name: {data.name}</h2>
-    </div>
+    <Card id={data.id}>
+      <h2>{data.name}</h2>
+      <p>Searches: {data.searches}</p> 
+      <p>Country: {data.country}</p>
+    </Card>
   );
 }
 
--- client/src/components/PlayerRanking.jsx
+++ client/src/components/PlayerRanking.jsx
@@ -2,6 +2,32 @@ import React from 'react';
 import useData from './../hooks/useData';
 import PlayerCard from './PlayerCard';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Cards = styled.div`
+  background: url("https://images.unsplash.com/photo-1552667466-07770ae110d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 75%
+  
+  @media (max-width: 1000px) {
+    width: 80%;
+  }
+
+  @media (max-width: 700px) {
+    width: 90%;
+  }
+
+  @media (max-width: 400px) {
+    width: 95%;
+  }
+`
 
 class PlayerRanking extends React.Component {
   constructor() {
@@ -26,7 +52,7 @@ class PlayerRanking extends React.Component {
 
   render () {
     return (
-      <div>
+      <Cards>
         {
           this.state.players.length > 0 ? 
             this.state.players.map(player => (
@@ -34,7 +60,7 @@ class PlayerRanking extends React.Component {
             ))
             : null
         }
-      </div>
+      </Cards>
     );
   }
 }
